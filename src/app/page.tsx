fcab286949fb2f0d8dcebef3a328c34618cb3753
Pedro Costa <pedro4costa@outlook.com>
2024-02-06 09:59:18 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+/*import { EmojiRating } from "./Components/EmojiRating";
+
+const Page = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <EmojiRating rate={2.8}/>
+    </div>
+  );
+}
+
+export default Page;*/
+import { EmojiRating } from "./Components/EmojiRating";
+
+const Page = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <EmojiRating rate={5.8}/>
+    </div>
+  );
+}
+
+export default Page;
